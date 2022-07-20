0340d0ea4b0fedfd1c6124abf10c56c09ed73d89
Mihai Toader <mihai@sectorlabs.ro>
2022-07-20 23:57:08 +0300

    aha aha

--- verification-check/main.py
+++ verification-check/main.py
@@ -2,8 +2,6 @@ import requests
 import os
 
 print(os.environ['WORKFLOWS_TOKEN'])
-WORKFLOWS_TOKEN = os.environ['WORKFLOWS_TOKEN']
-
 
 merged_prs = requests.get('https://api.github.com/search/issues', params={'q': 'is:pull-request'}, headers={'Authorization': f'Token {WORKFLOWS_TOKEN}'})
 print(merged_prs)
